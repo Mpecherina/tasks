a52b480bba66143b7438137ceb524478860b10f5
Mpecherina <lumandra@yandex.ru>
2017-07-26 14:40:00 +0300

    each instead for

--- analyze.rb
+++ analyze.rb
@@ -2,15 +2,11 @@
 
 lines = File.readlines("/home/akim/Task_one/mddrive.xml")
 
-for i in 0..lines.length
-  
-  if /template name/.match(lines[i])
+File.open("/home/akim/Task_one/mddrive.txt", "w"){ |f| lines.each { |i| f.puts i.split(/"/)[1] if /template name/.match(i)}}
 
-    File.open("/home/akim/Task_one/mddrive.txt", "a"){|f| f.puts lines[i].split(/"/)[1]}
 
-  end
+
  
-end
     
   
   
